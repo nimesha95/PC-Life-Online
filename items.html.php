ad3809f17a04290532407b2190b7fb3089b10f66
nimesha95 <nimesha95@live.com>
2017-08-16 22:57:48 +0530

    added header as require.php file

--- items.html.php
+++ items.html.php
@@ -8,9 +8,13 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+    <link rel="stylesheet" type="text/css" href="css/Style1.css">
+
 </head>
 <body>
 <?php
+include 'header.inc.php';
+
 
 $db = $_GET['item']; //setting the _GET variable which recieved from the index page
 
