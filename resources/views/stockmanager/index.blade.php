c425f0879c859855f26105cf747cf275a8a625b9
nimesha95 <nimesha95@live.com>
2017-11-25 01:48:13 +0530

    added a chart to use in future (hardcoded)

--- resources/views/stockmanager/index.blade.php
+++ resources/views/stockmanager/index.blade.php
@@ -6,8 +6,40 @@
 
 @section('header')
     @include('partials.stock_header')
+
+    <script>
+        window.onload = function () {
+
+            var chart = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: true,
+                title: {
+                    text: "Email Categories",
+                    horizontalAlign: "left"
+                },
+                data: [{
+                    type: "doughnut",
+                    startAngle: 60,
+                    //innerRadius: 60,
+                    indexLabelFontSize: 17,
+                    indexLabel: "{label} - #percent%",
+                    toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+                    dataPoints: [
+                        {y: 67, label: "Inbox"},
+                        {y: 28, label: "Archives"},
+                        {y: 10, label: "Labels"},
+                        {y: 7, label: "Drafts"},
+                        {y: 15, label: "Trash"},
+                        {y: 6, label: "Spam"}
+                    ]
+                }]
+            });
+            chart.render();
+
+        }
+    </script>
 @endsection
 
 @section('content')
-    <a href="">Add Product</a>
+    <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 @endsection
