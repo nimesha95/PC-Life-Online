361ac181f89f285e44cc4a0cc7816bc0183d44eb
nimesha95 <nimesha95@live.com>
2017-12-09 00:11:07 +0530

    deliveries are added to firebase database

---
+++ app/Mail/DeliveryAccepted.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class DeliveryAccepted extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('info@pclife.com')
+            ->view('emails.DeliveryAccept');
+    }
+}
